a3e20f4a9ced7c4e7a5d62858bf346eb03476e48
shogenji <ruirui@ruirui.jp>
2023-06-10 10:17:05 +0900

    更新 main.blocks, runkit.ts

--- runkit.ts
+++ runkit.ts
@@ -1,10 +1,10 @@
 /**
  * Extensions for Switch Education's "Run Your micro:bit Kit."
  */
-
-
 //% weight=100 color=#ff7700 icon="\uf1b9"
 //% block="Run Kit"
+
+
 namespace runkit {
 
     let speedMax = 512
